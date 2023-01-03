112555fa9826c54694f98b3b1dd73132942311d5
YOHANNES KANKO <johanneskanko7@gmail.com>
2023-01-03 04:48:13 -0800

    Update state

--- models/state.py
+++ models/state.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Review module for the HBNB project """
+import os
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    place_id = Column(
+        String(60), ForeignKey('places.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    user_id = Column(
+        String(60), ForeignKey('users.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    text = Column(
+        String(1024), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
