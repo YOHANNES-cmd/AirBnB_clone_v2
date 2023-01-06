339830a3a0b904b14f3e5290f96f1becf673e30d
YOHANNES KANKO <johanneskanko7@gmail.com>
2023-01-06 03:56:04 -0800

    Update revert

--- models/state.py
+++ models/state.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
+""" State Module for HBNB project """
 import os
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from models.base_model import BaseModel, Base
+from models.city import City
 
 
-class Review(BaseModel, Base):
-    """ Review classto store review information """
-    __tablename__ = 'reviews'
-    place_id = Column(
-        String(60), ForeignKey('places.id'), nullable=False
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+    name = Column(
+        String(128), nullable=False
     ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    user_id = Column(
-        String(60), ForeignKey('users.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    text = Column(
-        String(1024), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(
+            'City',
+            cascade='all, delete, delete-orphan',
+            backref='state'
+        )
+    else:
+        @property
+        def cities(self):
+            """Returns the cities in this State"""
+            from models import storage
+            cities_in_state = []
+            for value in storage.all(City).values():
+                if value.state_id == self.id:
+                    cities_in_state.append(value)
+            return cities_in_state
