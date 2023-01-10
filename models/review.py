1e9f95ed7b9240d45b83653675cd4f6da7a6b5d0
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-01-10 17:42:47 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -17,7 +17,12 @@ class Review(BaseModel, Base):
         place_id (sqlalchemy String): The review's place id.
         user_id (sqlalchemy String): The review's user id.
     """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
