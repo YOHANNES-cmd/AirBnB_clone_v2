ce8773a905a0c10faf5cbbd5a7ce6df6cde4cf8e
fasil729 <101599549+fasil729@users.noreply.github.com>
2023-01-10 17:50:32 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
