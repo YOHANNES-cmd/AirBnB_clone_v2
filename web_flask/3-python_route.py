56532fbf234fdd91b241fe430a92b48f77aa81cf
Yohannes Kanko <johanneskanko7@gmail.com>
2023-02-05 13:15:56 +0000

    3. Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,5 +27,12 @@ def c_page(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
+@app.route('/python/<text>')
+@app.route('/python', defaults={'text': 'is cool'})
+def python_page(text):
+    '''The python page.'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
